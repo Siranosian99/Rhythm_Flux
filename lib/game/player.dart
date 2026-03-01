1dd44167ee296aa19bcb1007918c8cef9bb9327c
Siranosian99 <aleppo.vs1@gmail.com>
2026-03-01 09:07:19 +0400

    working

--- lib/game/player.dart
+++ lib/game/player.dart
@@ -2,10 +2,12 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/geometry.dart';
 import 'package:flame/extensions.dart';
+import 'package:rhythm_flux/game/game_screen.dart';
 
-class Player extends SpriteComponent with TapCallbacks {
+class Player extends SpriteComponent with TapCallbacks , HasGameRef<MyGame>{
   Player({super.position}) :
         super(size: Vector2.all(150), anchor: Anchor.center );
+  double speed = -100; // yukarı gidiyor
 
   @override
   Future<void> onLoad() async {
@@ -14,12 +16,21 @@ class Player extends SpriteComponent with TapCallbacks {
 
   @override
   void onTapUp(TapUpEvent info) {
-    size += Vector2.all(50);
+    // size += Vector2.all(50);
+    speed  += speed++;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    position.y -= 100 * dt;
-  }
+    position.y+= speed * dt;
+    if(position.y - size.y / 2 <=0){
+      position.y = size.y / 2;
+      speed = 100;
+    }
+    if(position.y + size.y / 2 >= gameRef.size.y){
+      position.y = gameRef.size.y - size.y / 2;
+      speed= -100;
+    }
+    }
 }
